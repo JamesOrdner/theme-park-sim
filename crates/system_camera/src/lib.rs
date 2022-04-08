05d902c98c2853b2a60d55fc9e7af77febc4565a
JamesOrdner <5782729+JamesOrdner@users.noreply.github.com>
2022-04-08 12:52:55 +0200

    camera damping

--- crates/system_camera/src/lib.rs
+++ crates/system_camera/src/lib.rs
@@ -1,4 +1,4 @@
-use std::f32::consts::{FRAC_PI_2, TAU};
+use std::f32::consts::FRAC_PI_2;
 
 use event::{AsyncEventDelegate, InputEvent};
 use frame_buffer::{CameraInfo, FrameBufferWriter};
@@ -6,8 +6,11 @@ use nalgebra_glm::{vec3, Vec3};
 
 pub struct FrameData {
     origin: Vec3,
+    origin_vel: Vec3,
     azimuth_angle: f32,
+    azimuth_angle_target: f32,
     polar_angle: f32,
+    polar_angle_target: f32,
     boom_len: f32,
 }
 
@@ -15,14 +18,20 @@ impl Default for FrameData {
     fn default() -> Self {
         Self {
             origin: Default::default(),
+            origin_vel: Default::default(),
             azimuth_angle: 0.0,
+            azimuth_angle_target: 0.0,
             polar_angle: 0.0,
+            polar_angle_target: 0.0,
             boom_len: 5.0,
         }
     }
 }
 
-const CAMERA_SPEED: f32 = 3.0;
+const MOVE_SPEED: f32 = 3.0;
+const ROTATE_SPEED: f32 = 0.01;
+const MOVE_DAMPING_FACTOR: f32 = 0.001;
+const ROTATE_DAMPING_FACTOR: f32 = 0.00001;
 
 impl FrameData {
     pub async fn update(
@@ -34,22 +43,34 @@ impl FrameData {
         for input_event in event_delegate.input_events() {
             match input_event {
                 InputEvent::CameraMoveAxis(axis) => {
-                    let dir = nalgebra_glm::rotate_vec3(
+                    self.origin_vel += nalgebra_glm::rotate_vec3(
                         &vec3(axis.x, 0.0, axis.y),
-                        self.azimuth_angle,
+                        self.azimuth_angle_target,
                         &vec3(0.0, 1.0, 0.0),
                     );
-                    self.origin += dir * CAMERA_SPEED * delta_time;
+
+                    let norm = self.origin_vel.norm();
+                    if norm > 1.0 {
+                        self.origin_vel.unscale_mut(norm);
+                    }
                 }
                 InputEvent::CameraRotateAxis(axis) => {
-                    self.azimuth_angle += axis.x * 0.005;
-                    self.azimuth_angle %= TAU;
-                    self.polar_angle += axis.y * 0.01;
-                    self.polar_angle = self.polar_angle.max(0.05).min(FRAC_PI_2 - 0.05);
+                    self.azimuth_angle_target += axis.x * ROTATE_SPEED;
+                    self.polar_angle_target += axis.y * ROTATE_SPEED;
+
+                    self.polar_angle_target =
+                        self.polar_angle_target.max(0.05).min(FRAC_PI_2 - 0.05);
                 }
                 _ => {}
             }
         }
+
+        self.origin += self.origin_vel * MOVE_SPEED * delta_time;
+        self.origin_vel *= MOVE_DAMPING_FACTOR.powf(delta_time);
+
+        let rotate_alpha = 1.0 - ROTATE_DAMPING_FACTOR.powf(delta_time);
+        self.azimuth_angle += (self.azimuth_angle_target - self.azimuth_angle) * rotate_alpha;
+        self.polar_angle += (self.polar_angle_target - self.polar_angle) * rotate_alpha;
 
         let location = nalgebra_glm::rotate_vec3(
             &vec3(0.0, 0.0, -self.boom_len),
