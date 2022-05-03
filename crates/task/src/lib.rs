f74d87d5512d4e5e9ca95a57c3bdb468264d4077
JamesOrdner <5782729+JamesOrdner@users.noreply.github.com>
2022-05-03 16:37:14 -0500

    use async-lock RwLock for shared data. potential placeholder

--- crates/task/src/lib.rs
+++ crates/task/src/lib.rs
@@ -1,9 +1,6 @@
-use std::{
-    ops::{Deref, DerefMut},
-    sync::Arc,
-};
+use std::sync::Arc;
 
-use atomic_refcell::{AtomicRef, AtomicRefCell, AtomicRefMut};
+use async_lock::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 #[derive(Clone)]
 pub struct SharedData<T0 = (), T1 = (), const T1_LEN: usize = 0> {
@@ -11,8 +8,19 @@ pub struct SharedData<T0 = (), T1 = (), const T1_LEN: usize = 0> {
 }
 
 struct SharedDataImpl<T0 = (), T1 = (), const T1_LEN: usize = 0> {
-    single_data: AtomicRefCell<T0>,
-    multiple_data: [AtomicRefCell<T1>; T1_LEN],
+    single_data: RwLock<T0>,
+    multiple_data: [RwLock<T1>; T1_LEN],
+}
+
+impl<T0> SharedData<T0, (), 0> {
+    pub fn new_single(data: T0) -> Self {
+        let inner = Arc::new(SharedDataImpl {
+            single_data: RwLock::new(data),
+            multiple_data: [],
+        });
+
+        Self { inner }
+    }
 }
 
 impl<T0, T1, const T1_LEN: usize> Default for SharedData<T0, T1, T1_LEN>
@@ -22,52 +30,36 @@ where
 {
     fn default() -> Self {
         let inner = Arc::new(SharedDataImpl {
-            single_data: AtomicRefCell::new(Default::default()),
-            multiple_data: [0; T1_LEN].map(|_| AtomicRefCell::new(Default::default())),
+            single_data: RwLock::new(Default::default()),
+            multiple_data: [0; T1_LEN].map(|_| RwLock::new(Default::default())),
         });
 
         Self { inner }
     }
 }
 
-impl<T0, T1, const T1_LEN: usize> SharedData<T0, T1, T1_LEN>
+impl<T1, const T1_LEN: usize> SharedData<(), T1, T1_LEN>
 where
-    T0: Default,
     T1: Copy,
 {
-    pub fn new_multiple_copy(data: T1) -> Self {
+    pub fn new_multiple(data: T1) -> Self {
         let inner = Arc::new(SharedDataImpl {
-            single_data: AtomicRefCell::new(Default::default()),
-            multiple_data: [0; T1_LEN].map(|_| AtomicRefCell::new(data)),
+            single_data: RwLock::new(()),
+            multiple_data: [0; T1_LEN].map(|_| RwLock::new(data)),
         });
 
         Self { inner }
     }
 }
 
-impl<T0, T1, const T1_LEN: usize> SharedData<T0, T1, T1_LEN>
+impl<T1, const T1_LEN: usize> SharedData<(), T1, T1_LEN>
 where
-    T0: Default,
     T1: Clone,
 {
     pub fn new_multiple_clone(data: T1) -> Self {
         let inner = Arc::new(SharedDataImpl {
-            single_data: AtomicRefCell::new(Default::default()),
-            multiple_data: [0; T1_LEN].map(|_| AtomicRefCell::new(data.clone())),
-        });
-
-        Self { inner }
-    }
-}
-
-impl<T0, T1, const T1_LEN: usize> SharedData<T0, T1, T1_LEN>
-where
-    T1: Default,
-{
-    pub fn new_single(data: T0) -> Self {
-        let inner = Arc::new(SharedDataImpl {
-            single_data: AtomicRefCell::new(data),
-            multiple_data: [0; T1_LEN].map(|_| AtomicRefCell::new(Default::default())),
+            single_data: RwLock::new(()),
+            multiple_data: [0; T1_LEN].map(|_| RwLock::new(data.clone())),
         });
 
         Self { inner }
@@ -77,64 +69,26 @@ where
 impl<T0, T1, const T1_LEN: usize> SharedData<T0, T1, T1_LEN> {
     pub fn new(single_data: T0, multiple_data: [T1; T1_LEN]) -> Self {
         let inner = Arc::new(SharedDataImpl {
-            single_data: AtomicRefCell::new(single_data),
-            multiple_data: multiple_data.map(|data| AtomicRefCell::new(data)),
+            single_data: RwLock::new(single_data),
+            multiple_data: multiple_data.map(|data| RwLock::new(data)),
         });
 
         Self { inner }
     }
 
-    pub async fn read_single(&self) -> SharedDataRef<'_, T0> {
-        SharedDataRef {
-            inner: self.inner.single_data.borrow(),
-        }
+    pub async fn read_single(&self) -> RwLockReadGuard<'_, T0> {
+        self.inner.single_data.read().await
     }
 
-    pub async fn write_single(&self) -> SharedDataRefMut<'_, T0> {
-        SharedDataRefMut {
-            inner: self.inner.single_data.borrow_mut(),
-        }
+    pub async fn write_single(&self) -> RwLockWriteGuard<'_, T0> {
+        self.inner.single_data.write().await
     }
 
-    pub async fn read_multiple(&self, offset: usize) -> SharedDataRef<'_, T1> {
-        SharedDataRef {
-            inner: self.inner.multiple_data[offset].borrow(),
-        }
+    pub async fn read_multiple(&self, offset: usize) -> RwLockReadGuard<'_, T1> {
+        self.inner.multiple_data[offset].read().await
     }
 
-    pub async fn write_multiple(&self, offset: usize) -> SharedDataRefMut<'_, T1> {
-        SharedDataRefMut {
-            inner: self.inner.multiple_data[offset].borrow_mut(),
-        }
-    }
-}
-
-pub struct SharedDataRef<'a, T> {
-    inner: AtomicRef<'a, T>,
-}
-
-impl<'a, T> Deref for SharedDataRef<'a, T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &*self.inner
-    }
-}
-
-pub struct SharedDataRefMut<'a, T> {
-    inner: AtomicRefMut<'a, T>,
-}
-
-impl<'a, T> Deref for SharedDataRefMut<'a, T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &*self.inner
-    }
-}
-
-impl<'a, T> DerefMut for SharedDataRefMut<'a, T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut *self.inner
+    pub async fn write_multiple(&self, offset: usize) -> RwLockWriteGuard<'_, T1> {
+        self.inner.multiple_data[offset].write().await
     }
 }
